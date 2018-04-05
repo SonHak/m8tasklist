39e7df7f892b02519ab5e3f94c1907b9e7f56d53
acardenaslara <acardenaslara@iesesteveterradas.cat>
2018-04-05 18:03:11 +0200

    proba de conexió 3

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@
     	$dbname = "dc1md9da6pqja0";
     	$username = "vfefuxixlzddch";
    		$pw = "65743a71b2332c4a84d161eb2f6ffae1f8e49b4ea3fc971d49d692c966a76b1f";
-    	$pdo = new PDO ("mysql:host=$hostname;port:5432;dbname=$dbname","$username","$pw");
+    	$pdo = new PDO ("mysql:host=$hostname;port=5432;dbname=$dbname","$username","$pw");
 	}catch (PDOException $e){
 		echo "Failed to get DB handle: " . $e->getMessage() . "\n";
     	exit;
