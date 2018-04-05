c9e0bd25cfb7ff920ff1f38f90701f7dace51fba
acardenaslara <acardenaslara@iesesteveterradas.cat>
2018-04-05 17:40:25 +0200

    proba de conexió

--- web/index.php
+++ web/index.php
@@ -1,3 +1,16 @@
 <?php
-	echo "hello world!";
+	include('connect.php');
+	
+	//Create table
+	$query = $pdo->prepare("CREATE TABLE IF NOT EXISTS 'tasklist'(
+							'id' INT AUTO_INCREMENT NOT NULL,
+							'task' varchar(100) NOT NULL,
+							'desc' TEXT,
+							'check' tinyint,
+			  			   )");
+
+	$query -> execute();
+
+
+
 ?>
