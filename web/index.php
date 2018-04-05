19aa2c50a8f506f6594fb640c4ad2fdbc27e49f3
acardenaslara <acardenaslara@iesesteveterradas.cat>
2018-04-05 18:00:36 +0200

    proba de conexió

--- web/index.php
+++ web/index.php
@@ -1,7 +1,15 @@
 <?php
-	if(include('connect.php')){
-		echo "hello world!";	
+	try{
+		$hostname = "ec2-54-163-240-54.compute-1.amazonaws.com";
+    	$dbname = "dc1md9da6pqja0";
+    	$username = "vfefuxixlzddch";
+   		$pw = "65743a71b2332c4a84d161eb2f6ffae1f8e49b4ea3fc971d49d692c966a76b1f";
+    	$pdo = new PDO ("mysql:host=$hostname;dbname=$dbname","$username","$pw");
+	}catch (PDOException $e){
+		echo "Failed to get DB handle: " . $e->getMessage() . "\n";
+    	exit;
 	}
+	/*
 	crearTabla();
 	insertarDatos("Tarea1","Esta es la primera tarea",1);
 
@@ -41,5 +49,5 @@
 
 
 
-
+*/
 ?>
