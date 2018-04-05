f613d24632c7e07b859f520d0bb4489e7fc8a790
acardenaslara <acardenaslara@iesesteveterradas.cat>
2018-04-05 17:55:03 +0200

    proba de conexió

--- web/index.php
+++ web/index.php
@@ -1,7 +1,21 @@
 <?php
 	include('connect.php');
 	echo "hello world!";	
-	
+	crearTabla();
+	insertarDatos("Tarea1","Esta es la primera tarea",1);
+
+
+	$query = $pdo->prepare("select * FROM tasklist");
+	$query->execute();
+
+	//anem agafant les fileres d'amb una amb una
+	$row = $query->fetch();
+	while ( $row ) {
+	echo $row['task']." - " . $row['desc']." - ".$row['check']."<br/>";
+	$row = $query->fetch();
+	}
+
+
 
 	//Create table
 	function crearTabla(){
@@ -11,12 +25,20 @@
 								'desc' TEXT,
 								'check' tinyint,
   			   				  )");
-		$query -> execute();
+		$query->execute();
 	};
 
 	//Insert date
-	#function insertarDatos(){
-#
-	#}
+	function insertarDatos($tarea,$descrip,$hecho){
+		$query = $pdo->prepare("INSERT INTO tasklist VALUES(:task, :desc, :check)");
+		$query->execute(array(
+			"task" => $tarea,
+			"desc" => $descrip,
+			"check" => $hecho
+		));
+	}
+
+
+
 
 ?>
