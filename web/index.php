2110b9b14e2d57b2ae8504c8a80fcb8e46ff7626
acardenaslara <acardenaslara@iesesteveterradas.cat>
2018-04-06 18:51:37 +0200

    selec

--- web/index.php
+++ web/index.php
@@ -4,11 +4,13 @@
     	$dbname = "dc1md9da6pqja0";
     	$username = "vfefuxixlzddch";
    		$pw = "65743a71b2332c4a84d161eb2f6ffae1f8e49b4ea3fc971d49d692c966a76b1f";
-    	$pdo = new PDO ("pgsql:host=$hostname;port=5432;dbname=$dbname","$username","$pw");
+    	$pdo = new PDO ("psql:host=$hostname;port=5432;dbname=$dbname","$username","$pw");
 	}catch (PDOException $e){
 		echo "Failed to get DB handle: " . $e->getMessage() . "\n";
     	exit;
 	}
+
+	/*
 	//crae la tabla
 	$query = $pdo->prepare("CREATE TABLE IF NOT EXISTS 'tasklist'(
 								'id' INT AUTO_INCREMENT NOT NULL,
@@ -27,14 +29,14 @@
 		"check" => 1
 	));
 
-
+	*/
 	$query = $pdo->prepare("select * FROM tasklist");
 	$query->execute();
 
 	//anem agafant les fileres d'amb una amb una
 	$row = $query->fetch();
 	while ( $row ) {
-	echo $row['task']." - " . $row['desc']." - ".$row['check']."<br/>";
+	echo $row['id']." - " . $row['task']."<br/>";
 	$row = $query->fetch();
 	}
 
